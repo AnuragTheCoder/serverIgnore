38366cdb30af1853d6b91fd9c2838066f9de0f2e
AnuragTheCoder <129210052+AnuragTheCoder@users.noreply.github.com>
2023-12-19 19:48:52 +0530

    Added async Functionality

--- newIndex.js
+++ newIndex.js
@@ -1,1 +1,12 @@
 "nothing here"
+
+
+const myPromise=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+resolve("Hello")
+    },2000)
+})
+
+const myFunc=async()=>{
+    const data=await myPromise;
+}
